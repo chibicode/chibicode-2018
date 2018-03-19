a2b11a80a0a0abe3a665e6264ea1de5c6d27df60
Shu Uesugi <shu@chibicode.com>
2018-03-19 01:04:07 -0700

    Remove typeface, add ts checker fork

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,7 @@ const _ = require('lodash')
 const Promise = require('bluebird')
 const path = require('path')
 const { createFilePath } = require('gatsby-source-filesystem')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
 exports.createPages = ({ graphql, boundActionCreators }) => {
   const { createPage } = boundActionCreators
@@ -12,7 +13,10 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
       graphql(
         `
           {
-            allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }, limit: 1000) {
+            allMarkdownRemark(
+              sort: { fields: [frontmatter___date], order: DESC }
+              limit: 1000
+            ) {
               edges {
                 node {
                   fields {
@@ -33,11 +37,12 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
         }
 
         // Create blog posts pages.
-        const posts = result.data.allMarkdownRemark.edges;
+        const posts = result.data.allMarkdownRemark.edges
 
         _.each(posts, (post, index) => {
-          const previous = index === posts.length - 1 ? false : posts[index + 1].node;
-          const next = index === 0 ? false : posts[index - 1].node;
+          const previous =
+            index === posts.length - 1 ? false : posts[index + 1].node
+          const next = index === 0 ? false : posts[index - 1].node
 
           createPage({
             path: post.node.fields.slug,
@@ -65,4 +70,13 @@ exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
       value,
     })
   }
+}
+
+exports.modifyWebpackConfig = ({ config }) => {
+  config.plugin('ForkTsCheckerWebpackPlugin', ForkTsCheckerWebpackPlugin, [
+    {
+      tsconfig: path.join(__dirname, './tsconfig.json'),
+      tslint: path.join(__dirname, './tslint.json'),
+    },
+  ])
 }
