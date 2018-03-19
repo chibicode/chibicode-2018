6acb33419fae0eefe98bfd3500b0b6106b0090b2
Shu Uesugi <shu@chibicode.com>
2018-03-19 02:20:53 -0700

    Remove index.old.js

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -3,7 +3,12 @@ import 'tachyons'
 import 'typeface-roboto-mono'
 import AppStyles from '../components/AppStyles'
 import PageHeader from '../components/PageHeader'
+// Links automatically get prefixed but if you want to compare
+// location use this https://github.com/gatsbyjs/gatsby/tree/master/packages/gatsby-link
+// e.g. const isHomepage = location.pathname === withPrefix("/")
+// import { withPrefix } from 'gatsby-link'
 
+// TODO: App gets location and children() as params
 const App = () => (
   <AppStyles>
     <PageHeader />
