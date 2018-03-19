a9c9694fb20cf9300f131e48a267837296eaec8e
Shu Uesugi <shu@chibicode.com>
2018-03-18 20:18:38 -0700

    Add ForkTsCheckerWebpackPlugin, tslint

--- static.config.js
+++ static.config.js
@@ -1,47 +1,48 @@
 import axios from 'axios'
 import path from 'path'
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
 
 // Paths Aliases defined through tsconfig.json
 const typescriptWebpackPaths = require('./webpack.config.js')
 
 export default {
   entry: path.join(__dirname, 'src', 'index.tsx'),
-  getSiteData: () => ({
-    title: 'React Static',
-  }),
   getRoutes: async () => {
     const { data: posts } = await axios.get(
       'https://jsonplaceholder.typicode.com/posts',
     )
     return [
       {
-        path: '/',
         component: 'src/containers/Home',
+        path: '/',
       },
       {
-        path: '/about',
         component: 'src/containers/About',
+        path: '/about',
       },
       {
-        path: '/blog',
-        component: 'src/containers/Blog',
-        getData: () => ({
-          posts,
-        }),
         children: posts.map(post => ({
-          path: `/post/${post.id}`,
           component: 'src/containers/Post',
           getData: () => ({
             post,
           }),
+          path: `/post/${post.id}`,
         })),
+        component: 'src/containers/Blog',
+        getData: () => ({
+          posts,
+        }),
+        path: '/blog',
       },
       {
-        is404: true,
         component: 'src/containers/404',
+        is404: true,
       },
     ]
   },
+  getSiteData: () => ({
+    title: 'React Static',
+  }),
   webpack: (config, { defaultLoaders }) => {
     // Add .ts and .tsx extension to resolver
     config.resolve.extensions.push('.ts', '.tsx')
@@ -56,8 +57,8 @@ export default {
       {
         oneOf: [
           {
-            test: /\.(js|jsx|ts|tsx)$/,
             exclude: defaultLoaders.jsLoader.exclude, // as std jsLoader exclude
+            test: /\.(js|jsx|ts|tsx)$/,
             use: [
               {
                 loader: 'babel-loader',
@@ -75,6 +76,14 @@ export default {
         ],
       },
     ]
+
+    config.plugins = (config.plugins || []).concat([
+      new ForkTsCheckerWebpackPlugin({
+        tsconfig: path.join(__dirname, './tsconfig.json'),
+        tslint: path.join(__dirname, './tslint.json'),
+      }),
+    ])
+
     return config
   },
 }
