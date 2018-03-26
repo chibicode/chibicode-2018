1df5e1a931f060817edd0554523b1b5ecb6bb031
Shu Uesugi <shu@chibicode.com>
2018-03-25 23:32:56 -0700

    Avoid build time error

--- scripts/fixApollo.js
+++ scripts/fixApollo.js
@@ -3,3 +3,10 @@ const shell = require('shelljs')
 shell.ls('./src/**/__generated__/*.ts').forEach(file => {
   shell.sed('-i', 'date: Date', 'date: string', file)
 })
+
+// If there are files under pages directory that would cause error on gatsby build
+// https://github.com/gatsbyjs/gatsby/issues/1499#issuecomment-376059629
+shell.ls('./src/**/__generated__/*.ts').forEach(file => {
+  shell.cp('-f', file, './src/types/')
+  shell.rm(file)
+})
