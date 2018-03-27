266c6b407a6983d33f51c76db8b43f8740abf1a0
Shu Uesugi <shu@chibicode.com>
2018-03-27 05:10:10 -0700

    post separator turns into an hr tag

--- src/components/PostSeparator.tsx
+++ src/components/PostSeparator.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { HrTag } from '../components/BlockElements'
 import { POST_SEPARATOR_DISPLAY_NAME } from '../constants/components'
 
-const PostSeparator: React.SFC<{}> = () => <span />
+const PostSeparator: React.SFC<{}> = () => <HrTag />
 
 PostSeparator.displayName = POST_SEPARATOR_DISPLAY_NAME
 
